e75f1a46aa05d27c2e27f5ac627989d69cbe8944
Viktoria Andrushenka <vika.sapriko.9981@gmail.com>
2025-03-30 23:22:53 +0300

    add models and migration

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Наименование продукта")
+    description = models.TextField(max_length=250, verbose_name="Описание продукта")
+    catalog_image = models.ImageField(upload_to="images/", blank=True, null=True)
+    category = models.ForeignKey(
+        "Category",
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        blank=True,
+        null=True,
+        related_name="products",
+    )
+    cost = models.IntegerField(verbose_name="Цена за покупку")
+    created_at = models.DateField(verbose_name="Дата создания")
+    updated_at = models.DateField(verbose_name="Дата последнего изменения")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "category"]
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Наименование категории")
+    description = models.TextField(max_length=250, verbose_name="Описание категории", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
