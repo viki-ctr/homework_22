f0ee502ab4a09d5125ebe4bf315e466af2de9c2f
viki-ctr <vika.sapriko.9981@gmail.com>
2025-03-31 00:41:32 +0300

    Revert "Feature/homework 23"

--- config/settings.py
+++ config/settings.py
@@ -10,13 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-import os
-from dotenv import load_dotenv
 from pathlib import Path
 
 from django.conf.global_settings import STATICFILES_DIRS
-
-load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "django-insecure-u1pnx#9(l_=kcmcg-l$3!u%r@+r2eyrognq5h^au!!=mpw5w!0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  True if os.getenv('DEBUG') == 'True' else False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -82,12 +78,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv('NAME'),
-        "USER": os.getenv('USER'),
-        "PASSWORD": os.getenv('PASSWORD'),
-        "HOST": os.getenv('HOST'),
-        "PORT": os.getenv('PORT'),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
