6a35dcf903be3f05f22b6b70bb7c2f6bc2f3e7bc
Viktoria Andrushenka <vika.sapriko.9981@gmail.com>
2025-04-30 22:26:53 +0300

    update homework

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, TemplateView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Product
@@ -32,10 +33,11 @@ class ProductCreateView(CreateView):
     success_url = reverse_lazy('catalog:product_list')
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
+    login_url = '/login/'
     success_url = reverse_lazy('catalog:product_list')
 
 
--- config/settings.py
+++ config/settings.py
@@ -140,3 +140,4 @@ MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_CHARSET = 'utf-8'
+LOGIN_REDIRECT_URL = '/products/'
