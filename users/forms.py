2856305bbe4071f1c5562ce19a18ceccc37c62e1
Viktoria Andrushenka <vika.sapriko.9981@gmail.com>
2025-05-02 01:05:25 +0300

    homework_27

---
+++ users/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import User
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(label='Email')
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Подтверждение пароля', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2')
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.EmailField(label='Email')
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
